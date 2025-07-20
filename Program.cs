5a5d22d3e99fae1543f02a2b5d4ca7544b6dc943
Sufyaan <sufyaan323@gmail.com>
2025-07-20 17:19:36 +0100

    Created a basic foundation for the script

--- Program.cs
+++ Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static string Prompt(string message)
+{
+    Console.Write(message);
+    return Console.ReadLine() ?? "";
+}
+
+static string GetName(int lineNumber)
+{
+    string? line = null;
+
+    using var reader = new StreamReader("names.txt");
+
+    for (int i = 0; i <= lineNumber; i++)
+    {
+        line = reader.ReadLine();
+    }
+
+    return line!;
+}
+
+string name = "";
+bool accepted = false;
+int quantity = 0;
+Random rand = new Random();
+
+while (accepted == false)
+{
+        try
+    {
+        string input = Prompt("How many names would you like to generate for the name? ");
+        quantity = string.IsNullOrWhiteSpace(input) ? 2 : int.Parse(input);
+        accepted = true;
+    }
+    catch
+    {
+        Console.WriteLine("Please enter a number.");
+    }
+}
+
+for (int i = 0; i < quantity; i++)
+{
+    int num = rand.Next(300);
+    name += GetName(num) + " ";
+}
+
+
+Console.WriteLine("Generated name: " + name.Trim());
